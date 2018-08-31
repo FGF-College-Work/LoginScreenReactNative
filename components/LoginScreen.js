79f2d23f7a077a2d5866deb993b42daf61841daa
marcialwushu <marcialwushu@gmail.com>
2018-08-31 17:39:51 -0300

    LoginScreen initial info

---
+++ components/LoginScreen.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  Animated,
+  Easing,
+} from 'react-native';
+import {Actions, ActionConst} from 'react-native-router-flux';
+
+
+export default class LoginScreen extends Component {
+    render() {
+        return(
+            <View></View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        margin: 20,
+    },
+});
